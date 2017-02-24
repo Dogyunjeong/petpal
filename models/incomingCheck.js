03a004be2e5dc2fdf4f647b460bd697ae1e4fc59
Dogyun Jeong <dogun098@gmail.com>
2017-02-24 12:41:10 +0900

    2017-02-17 18:40  finish API until 13

--- models/incomingCheck.js
+++ models/incomingCheck.js
@@ -6,6 +6,8 @@ function incomingCheck(req, res, next) {
       winstonlogger.log('debug', 'params     :  %j', req.params);
    if (Object.keys(req.body).length > 0)
       winstonlogger.log('debug', 'body       :  %j', req.body);
+   if (Object.keys(req.query).length > 0)
+      winstonlogger.log('debug', 'query     :  %j', req.query);
    if (req.file)
       winstonlogger.log('debug', 'file       :  %s', req.file.location);
    next();
