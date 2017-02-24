9d8aad2423a9e01607b567ca8d91d7c99b83a90d
Dogyun Jeong <dogun098@gmail.com>
2017-02-24 12:38:30 +0900

    2017-02-17 18:40  finish API until 13

--- routes/oauth.js
+++ routes/oauth.js
@@ -3,11 +3,13 @@ var router = express.Router();
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
+var KakaoStrategy = require('passport-kakao').Strategy;
+
 
 
 var logger = require('../common/logger');
 var dummy = require('../models/dummy');
-var kakaoConfig = require('../config/kakao_REST');
+var kakaoConfig = require('../config/kakao_oauth');
 var Oauth = require('../models/oauth');
 
 // passport.use(new BearerStrategy(
@@ -20,14 +22,14 @@ var Oauth = require('../models/oauth');
 //    }
 // ));
 
-passport.use(new LocalStrategy({usernameField: 'kakao_id', passwordField: 'kakao_token'}, function(kakao_id, kakao_token, done) {
-      Oauth.authorizeKakao(kakao_id, kakao_token, function(err, user) {
-         if (err) {
-            return done(err);
-         }
-         return done(null, user);
-      });
-   }));
+// passport.use(new LocalStrategy({usernameField: 'kakao_id', passwordField: 'kakao_token'}, function(kakao_id, kakao_token, done) {
+//       Oauth.authorizeKakao(kakao_id, kakao_token, function(err, user) {
+//          if (err) {
+//             return done(err);
+//          }
+//          return done(null, user);
+//       });
+//    }));
 
 passport.serializeUser(function (user, done){
    done(null, user.kakao_id);
@@ -41,56 +43,84 @@ passport.deserializeUser(function (kakao_id, done){
    });
 });
 
-router.post('/kakaotalk/token', function (req, res, next) {
-   if (!req.body.kakao_id || !req.body.kakao_token || !req.body.kakao_img_url ){
-      var  err = new Error('필수 데이터가 오지 않았습니다.');
-      err.status = 400;
-      return next(err);
+passport.use(new KakaoStrategy({
+      clientID : '48904c39ecdf2c8eb1641c8d97d615bf',
+      callbackURL : '카카오에 등록 한 주소'
+   }, function (accessToken, refreshToken, profile, done) {
+      var profile = profile._json;
+      Member.findOnCreate(profile, accessToken, function (err, member) { // 파라미터로 객체를 넘겨주는걸 파라미터 오브젝트라고함.
+
+         if (err)
+            return done(err);
+         done(null, member);
+      })
    }
-   var reqUser = {
-      kakao_id: req.body.kakao_id,
-      kakao_token: req.body.kakao_token,
-      kakao_img_url: req.body.kakao_img_url
-   };
+));
 
-   Oauth.findKakaoUserAndCreate(reqUser, function (err, user) {
-      if (err)
-         next(err);
-      if (user.reqJoinFlag) {
-         req.body.reqJoinFlag = user.reqJoinFlag;
-         req.body.user_id = user.user_id;
-      }
-      passport.authenticate('local', function (err, user) {
-         if (err || !user) {
-            return next(err);
-         }
-         req.login(user, function (err) {
-            if (err) {
-               return next(err);
-            }
-            next();
-         });
-      })(req, res, next);
-   });
+router.get("/kakao", passport.authenticate('kakao',{state: "myStateValue"}));
 
-}, function (req, res, next) {
-   var resultMsg = "회원 가입이 필요한 사용자입니다.";
-   var errMsg = "회원가입 및 로그인에 실패하였습니다.";
+router.get("/kakao/oauth", passport.authenticate('kakao'), function(req, res){
+   // 로그인 시작시 state 값을 받을 수 있음
 
-   if (req.body.reqJoinFlag) {
-      res.status(201).json({
-         result : {
-            message: "회원 가입이 필요한 사용자입니다."
-         }
-      });
-   } else {
-      res.json({
-         result : {
-            message: "등록된 사용자입니다."
-         }
-      });
-   }
+   res.send("callback state :" + req.query.state);
 });
+
+router.get('/kakaotalk/token', passport.authenticate('kakao-token'), function (req, res) {
+   res.send(req.user? '성공' : '실패');
+});
+
+
+
+// router.post('/kakaotalk/token', function (req, res, next) {
+//    if (!req.body.kakao_id || !req.body.kakao_token || !req.body.kakao_img_url ){
+//       var  err = new Error('필수 데이터가 오지 않았습니다.');
+//       err.status = 400;
+//       return next(err);
+//    }
+//    var reqUser = {
+//       kakao_id: req.body.kakao_id,
+//       kakao_token: req.body.kakao_token,
+//       kakao_img_url: req.body.kakao_img_url
+//    };
+//
+//    Oauth.findKakaoUserAndCreate(reqUser, function (err, user) {
+//       if (err)
+//          next(err);
+//       if (user.reqJoinFlag) {
+//          req.body.reqJoinFlag = user.reqJoinFlag;
+//          req.body.user_id = user.user_id;
+//       }
+//       passport.authenticate('local', function (err, user) {
+//          if (err || !user) {
+//             return next(err);
+//          }
+//          req.login(user, function (err) {
+//             if (err) {
+//                return next(err);
+//             }
+//             next();
+//          });
+//       })(req, res, next);
+//    });
+//
+// }, function (req, res, next) {
+//    var resultMsg = "회원 가입이 필요한 사용자입니다.";
+//    var errMsg = "회원가입 및 로그인에 실패하였습니다.";
+//
+//    if (req.body.reqJoinFlag) {
+//       res.status(201).json({
+//          result : {
+//             message: "회원 가입이 필요한 사용자입니다."
+//          }
+//       });
+//    } else {
+//       res.json({
+//          result : {
+//             message: "등록된 사용자입니다."
+//          }
+//       });
+//    }
+// });
 
 
 router.get('/logout', function (req,res,next) {
